1ae785804e85cd9db7033626801a54719bc6aace
Даниил Ешенко <91437231+Yawllen@users.noreply.github.com>
2025-07-17 12:42:28 +0300

    Update fullcalcNEW.py

--- Правильный расчет обьема/fullcalcNEW.py
+++ Правильный расчет обьема/fullcalcNEW.py
@@ -10,21 +10,69 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 loaded = []
 NAMESPACE = {'ns': 'http://schemas.microsoft.com/3dmanufacturing/core/2015/02'}
 
-# Материалы
 MATERIALS = {
-    "Enduse PETG": 1.27,
-    "Proto PLA": 1.24,
+    "Sealant TPU93": 1.20,
+    "Fiberpart ABS G4": 1.04,
+    "Fiberpart TPU C5": 1.20,
+    "Fiberpart ABSPA G8": 1.10,
+    "Fiberpart TPU G30": 1.20,
+    "Enduse SBS": 1.04,
     "Enduse ABS": 1.04,
+    "Enduse PETG": 1.27,
+    "Enduse PP": 0.90,
+    "Proto PLA": 1.24,
+    "ContiFiber CPA": 1.15,
+    "Sealant SEBS": 1.04,
+    "Sealant TPU": 1.20,
+    "Proto PVA": 1.19,
+    "Enduse-PA": 1.15,
+    "Enduse-TPU D70": 1.20,
+    "Fiberpart ABS G13": 1.04,
+    "Fiberpart PP G": 0.90,
+    "Fiberpart PP G30": 0.90,
+    "Enduse PC": 1.20,
+    "Fiberpart PA12 G12": 1.01,
+    "Metalcast-316L": 8.00,
+    "Fiberpart PC G20": 1.20,
+    "Fiberpart PA G30": 1.15,
+    "Enduse TPU D60": 1.20,
+    "Sealant TPU A90": 1.20,
+    "Sealant TPU A70": 1.20,
     "Fiberpart PA CF30": 1.15,
-    "Другой материал": 1.00
+    "Другой материал": 1.00,
 }
 
+# Price per gram for each material
 PRICE_PER_GRAM = {
-    "Enduse PETG": 2.33,
-    "Proto PLA": 3.07,
+    "Sealant TPU93": 3.75,
+    "Fiberpart ABS G4": 3.07,
+    "Fiberpart TPU C5": 5.6,
+    "Fiberpart ABSPA G8": 4.0,
+    "Fiberpart TPU G30": 4.0,
+    "Enduse SBS": 2.4,
     "Enduse ABS": 2.4,
+    "Enduse PETG": 2.33,
+    "Enduse PP": 8.08,
+    "Proto PLA": 3.07,
+    "ContiFiber CPA": 200.0,
+    "Sealant SEBS": 5.2,
+    "Sealant TPU": 5.98,
+    "Proto PVA": 9.5,
+    "Enduse-PA": 3.0,
+    "Enduse-TPU D70": 3.99,
+    "Fiberpart ABS G13": 6.65,
+    "Fiberpart PP G": 3.3,
+    "Fiberpart PP G30": 3.31,
+    "Enduse PC": 0.0,
+    "Fiberpart PA12 G12": 7.24,
+    "Metalcast-316L": 16.0,
+    "Fiberpart PC G20": 2.6,
+    "Fiberpart PA G30": 7.9,
+    "Enduse TPU D60": 1.9,
+    "Sealant TPU A90": 2.6,
+    "Sealant TPU A70": 6.4,
     "Fiberpart PA CF30": 10.4,
-    "Другой материал": 2.0
+    "Другой материал": 2.0,
 }
 
 # FDM параметры
